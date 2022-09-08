149f4aaadb5ed055a0b5309e863941d0499d6b5f
taylorjeandev <taylorgjean@gmail.com>
2022-09-08 22:28:58 +0200

    cleared home page

--- data/portfolios.ts
+++ data/portfolios.ts
@@ -1,189 +1,189 @@
 export const portfolio = [
-  {
-    imageUrl: "/dependency-report-optimized.gif",
-    imageAlt: "Dependencies report",
-    title: "Dependencies report",
-    description:
-      "Dependencies report aims to help analyze the consistency of the dependencies in your company's frontend projects.",
-    url: "https://github.com/candidosales/dependency-report",
-    techs: [
-      {
-        icon: "angular",
-        name: "Angular",
-      },
-      {
-        icon: "go",
-        name: "Go",
-      },
-      {
-        icon: "firebase",
-        name: "Firebase",
-      },
-      {
-        icon: "docker",
-        name: "Docker",
-      },
-    ],
-  },
-  {
-    imageUrl: "/cloud-function-go.png",
-    imageAlt: "API in GO to create Cloud Function",
-    title: "API in GO to create Cloud Function",
-    description: "🐹 An API for deploying Google Cloud Functions in Go.",
-    url: "https://github.com/candidosales/cloud-function-go",
-    techs: [
-      {
-        icon: "go",
-        name: "Go",
-      },
-      {
-        icon: "google-cloud",
-        name: "Google Cloud",
-      },
-    ],
-  },
-  {
-    imageUrl: "/material-time-picker.gif",
-    imageAlt: "Material Time Picker",
-    title: "Material Time Picker",
-    description: "A simple time picker component using Angular Material.",
-    url: "https://github.com/candidosales/material-time-picker",
-    techs: [
-      {
-        icon: "angular",
-        name: "Angular",
-      },
-      {
-        icon: "npm",
-        name: "NPM",
-      },
-    ],
-  },
-  {
-    imageUrl: "/figma-plugin-svelte-vite.png",
-    imageAlt: "Figma Plugin + Svelte + Vite + Typescript    ",
-    title: "Figma Plugin + Svelte + Vite + Typescript",
-    description:
-      "A boilerplate for creating Figma plugins with Svelte, Vite and Typescript",
-    url: "https://github.com/candidosales/figma-plugin-svelte-vite",
-    techs: [
-      {
-        icon: "figma",
-        name: "Figma",
-      },
-      {
-        icon: "svelte",
-        name: "Svelte",
-      },
-      {
-        icon: "vite",
-        name: "Vite",
-      },
-      {
-        icon: "typescript",
-        name: "Typescript",
-      },
-    ],
-  },
-  {
-    imageUrl: "/format-code.png",
-    imageAlt: "Format Code",
-    title: "Format Code",
-    description:
-      "Format Code makes it easy to format code snippets and add a theme for syntax highlight",
-    url: "https://github.com/candidosales/figma-format-code",
-    techs: [
-      {
-        icon: "typescript",
-        name: "Typescript",
-      },
-      {
-        icon: "sass",
-        name: "SASS",
-      },
-      {
-        icon: "webpack",
-        name: "Webpack",
-      },
-      {
-        icon: "figma",
-        name: "Figma",
-      },
-    ],
-  },
-  {
-    imageUrl: "/landing-page-book.png",
-    imageAlt: "Landing Page Book",
-    title: "Landing Page Book",
-    description:
-      "Landing page for advertising and sales of books to independent authors",
-    url: "https://github.com/candidosales/landing-page-book",
-    techs: [
-      {
-        icon: "sass",
-        name: "SASS",
-      },
-      {
-        icon: "bootstrap",
-        name: "Bootstrap",
-      },
-      {
-        icon: "webpack",
-        name: "Webpack",
-      },
-      {
-        icon: "netlify",
-        name: "Netlify",
-      },
-    ],
-  },
-  {
-    imageUrl: "/personal-blog.png",
-    imageAlt: "Personal Portfolio",
-    title: "Personal Portfolio",
-    description: "Landing page for your personal portfolio",
-    url: "https://github.com/candidosales/personal-blog",
-    techs: [
-      {
-        icon: "nextjs",
-        name: "NextJS",
-      },
-      {
-        icon: "react",
-        name: "React",
-      },
-      {
-        icon: "chakra-ui",
-        name: "Chakra UI",
-      },
-      {
-        icon: "sass",
-        name: "SASS",
-      },
-      {
-        icon: "vercel",
-        name: "Vercel",
-      },
-      {
-        icon: "pwa",
-        name: "PWA",
-      },
-    ],
-  },
-  {
-    imageUrl: "/empresas-angular.png",
-    imageAlt: "Empresas que usam Angular no Brasil",
-    title: "Empresas que usam Angular no Brasil",
-    description:
-      "Repository of companies in 🇧🇷 that use Angular in their projects",
-    url: "https://github.com/candidosales/empresas-que-usam-angular-no-brasil",
-    techs: [
-      {
-        icon: "github",
-        name: "Github",
-      },
-    ],
-  },
+  // {
+  //   imageUrl: "/dependency-report-optimized.gif",
+  //   imageAlt: "Dependencies report",
+  //   title: "Dependencies report",
+  //   description:
+  //     "Dependencies report aims to help analyze the consistency of the dependencies in your company's frontend projects.",
+  //   url: "https://github.com/candidosales/dependency-report",
+  //   techs: [
+  //     {
+  //       icon: "angular",
+  //       name: "Angular",
+  //     },
+  //     {
+  //       icon: "go",
+  //       name: "Go",
+  //     },
+  //     {
+  //       icon: "firebase",
+  //       name: "Firebase",
+  //     },
+  //     {
+  //       icon: "docker",
+  //       name: "Docker",
+  //     },
+  //   ],
+  // },
+  // {
+  //   imageUrl: "/cloud-function-go.png",
+  //   imageAlt: "API in GO to create Cloud Function",
+  //   title: "API in GO to create Cloud Function",
+  //   description: "🐹 An API for deploying Google Cloud Functions in Go.",
+  //   url: "https://github.com/candidosales/cloud-function-go",
+  //   techs: [
+  //     {
+  //       icon: "go",
+  //       name: "Go",
+  //     },
+  //     {
+  //       icon: "google-cloud",
+  //       name: "Google Cloud",
+  //     },
+  //   ],
+  // },
+  // {
+  //   imageUrl: "/material-time-picker.gif",
+  //   imageAlt: "Material Time Picker",
+  //   title: "Material Time Picker",
+  //   description: "A simple time picker component using Angular Material.",
+  //   url: "https://github.com/candidosales/material-time-picker",
+  //   techs: [
+  //     {
+  //       icon: "angular",
+  //       name: "Angular",
+  //     },
+  //     {
+  //       icon: "npm",
+  //       name: "NPM",
+  //     },
+  //   ],
+  // },
+  // {
+  //   imageUrl: "/figma-plugin-svelte-vite.png",
+  //   imageAlt: "Figma Plugin + Svelte + Vite + Typescript    ",
+  //   title: "Figma Plugin + Svelte + Vite + Typescript",
+  //   description:
+  //     "A boilerplate for creating Figma plugins with Svelte, Vite and Typescript",
+  //   url: "https://github.com/candidosales/figma-plugin-svelte-vite",
+  //   techs: [
+  //     {
+  //       icon: "figma",
+  //       name: "Figma",
+  //     },
+  //     {
+  //       icon: "svelte",
+  //       name: "Svelte",
+  //     },
+  //     {
+  //       icon: "vite",
+  //       name: "Vite",
+  //     },
+  //     {
+  //       icon: "typescript",
+  //       name: "Typescript",
+  //     },
+  //   ],
+  // },
+  // {
+  //   imageUrl: "/format-code.png",
+  //   imageAlt: "Format Code",
+  //   title: "Format Code",
+  //   description:
+  //     "Format Code makes it easy to format code snippets and add a theme for syntax highlight",
+  //   url: "https://github.com/candidosales/figma-format-code",
+  //   techs: [
+  //     {
+  //       icon: "typescript",
+  //       name: "Typescript",
+  //     },
+  //     {
+  //       icon: "sass",
+  //       name: "SASS",
+  //     },
+  //     {
+  //       icon: "webpack",
+  //       name: "Webpack",
+  //     },
+  //     {
+  //       icon: "figma",
+  //       name: "Figma",
+  //     },
+  //   ],
+  // },
+  // {
+  //   imageUrl: "/landing-page-book.png",
+  //   imageAlt: "Landing Page Book",
+  //   title: "Landing Page Book",
+  //   description:
+  //     "Landing page for advertising and sales of books to independent authors",
+  //   url: "https://github.com/candidosales/landing-page-book",
+  //   techs: [
+  //     {
+  //       icon: "sass",
+  //       name: "SASS",
+  //     },
+  //     {
+  //       icon: "bootstrap",
+  //       name: "Bootstrap",
+  //     },
+  //     {
+  //       icon: "webpack",
+  //       name: "Webpack",
+  //     },
+  //     {
+  //       icon: "netlify",
+  //       name: "Netlify",
+  //     },
+  //   ],
+  // },
+//   {
+//     imageUrl: "/personal-blog.png",
+//     imageAlt: "Personal Portfolio",
+//     title: "Personal Portfolio",
+//     description: "Landing page for your personal portfolio",
+//     url: "https://github.com/candidosales/personal-blog",
+//     techs: [
+//       {
+//         icon: "nextjs",
+//         name: "NextJS",
+//       },
+//       {
+//         icon: "react",
+//         name: "React",
+//       },
+//       {
+//         icon: "chakra-ui",
+//         name: "Chakra UI",
+//       },
+//       {
+//         icon: "sass",
+//         name: "SASS",
+//       },
+//       {
+//         icon: "vercel",
+//         name: "Vercel",
+//       },
+//       {
+//         icon: "pwa",
+//         name: "PWA",
+//       },
+//     ],
+//   },
+//   {
+//     imageUrl: "/empresas-angular.png",
+//     imageAlt: "Empresas que usam Angular no Brasil",
+//     title: "Empresas que usam Angular no Brasil",
+//     description:
+//       "Repository of companies in 🇧🇷 that use Angular in their projects",
+//     url: "https://github.com/candidosales/empresas-que-usam-angular-no-brasil",
+//     techs: [
+//       {
+//         icon: "github",
+//         name: "Github",
+//       },
+//     ],
+//   },
 ];
 
 export const extras = [
@@ -221,164 +221,164 @@ export const extras = [
   //     },
   //   ],
   // },
-  {
-    imageUrl: "/receituario-medico.png",
-    imageAlt: "Receituário Médico SUS",
-    title: "Receituário Médico SUS",
-    description:
-      "It helps doctors from the Brazilian public health system make medical prescriptions for free.",
-    url: "https://receituariomedico.app/",
-    techs: [
-      {
-        icon: "nextjs",
-        name: "NextJS",
-      },
-      {
-        icon: "chakra-ui",
-        name: "Chakra UI",
-      },
-      {
-        icon: "supabase",
-        name: "Supabase",
-      },
-      {
-        icon: "upstash",
-        name: "Upstash",
-      },
-      {
-        icon: "vercel",
-        name: "Vercel",
-      },
-      {
-        icon: "pwa",
-        name: "PWA",
-      },
-    ],
-  },
-  {
-    imageUrl: "/my-agenda-cultural.gif",
-    imageAlt: "Virada Cultural",
-    title: "My Cultural Agenda",
-    description:
-      "PWA website that helps you organize your agenda at large events",
-    url: "https://carnaval-sp-ec736.web.app/",
-    techs: [
-      {
-        icon: "angular",
-        name: "Angular",
-      },
-      {
-        icon: "firebase",
-        name: "Firebase",
-      },
-      {
-        icon: "ruby",
-        name: "Ruby",
-      },
-      {
-        icon: "rails",
-        name: "Rails",
-      },
-      {
-        icon: "pwa",
-        name: "PWA",
-      },
-    ],
-  },
+  // {
+  //   imageUrl: "/receituario-medico.png",
+  //   imageAlt: "Receituário Médico SUS",
+  //   title: "Receituário Médico SUS",
+  //   description:
+  //     "It helps doctors from the Brazilian public health system make medical prescriptions for free.",
+  //   url: "https://receituariomedico.app/",
+  //   techs: [
+  //     {
+  //       icon: "nextjs",
+  //       name: "NextJS",
+  //     },
+  //     {
+  //       icon: "chakra-ui",
+  //       name: "Chakra UI",
+  //     },
+  //     {
+  //       icon: "supabase",
+  //       name: "Supabase",
+  //     },
+  //     {
+  //       icon: "upstash",
+  //       name: "Upstash",
+  //     },
+  //     {
+  //       icon: "vercel",
+  //       name: "Vercel",
+  //     },
+  //     {
+  //       icon: "pwa",
+  //       name: "PWA",
+  //     },
+  //   ],
+  // },
+  // {
+  //   imageUrl: "/my-agenda-cultural.gif",
+  //   imageAlt: "Virada Cultural",
+  //   title: "My Cultural Agenda",
+  //   description:
+  //     "PWA website that helps you organize your agenda at large events",
+  //   url: "https://carnaval-sp-ec736.web.app/",
+  //   techs: [
+  //     {
+  //       icon: "angular",
+  //       name: "Angular",
+  //     },
+  //     {
+  //       icon: "firebase",
+  //       name: "Firebase",
+  //     },
+  //     {
+  //       icon: "ruby",
+  //       name: "Ruby",
+  //     },
+  //     {
+  //       icon: "rails",
+  //       name: "Rails",
+  //     },
+  //     {
+  //       icon: "pwa",
+  //       name: "PWA",
+  //     },
+  //   ],
+  // },
 ];
 
 export const volunteer = [
-  {
-    imageUrl: "/geleia-total.gif",
-    imageAlt: "Geleia Total",
-    title: "Geleia Total",
-    description: "Promote the culture and art of the state of Piauí - 🇧🇷",
-    url: "https://www.geleiatotal.com.br/",
-    techs: [
-      {
-        icon: "wordpress",
-        name: "Wordpress",
-      },
-      {
-        icon: "php",
-        name: "PHP",
-      },
-      {
-        icon: "digitalocean",
-        name: "Digital Ocean",
-      },
-      {
-        icon: "pwa",
-        name: "PWA",
-      },
-    ],
-  },
-  {
-    imageUrl: "/mapa-aborto-legal.gif",
-    imageAlt: "Mapa Aborto Legal",
-    title: "Mapa Aborto Legal",
-    description:
-      "All about legal abortion in 🇧🇷 - What situations are allowed, what is needed, which hospitals to turn to and more information",
-    url: "https://mapaabortolegal.org/",
-    techs: [
-      {
-        icon: "wordpress",
-        name: "Wordpress",
-      },
-      {
-        icon: "php",
-        name: "PHP",
-      },
-    ],
-  },
+  // {
+  //   imageUrl: "/geleia-total.gif",
+  //   imageAlt: "Geleia Total",
+  //   title: "Geleia Total",
+  //   description: "Promote the culture and art of the state of Piauí - 🇧🇷",
+  //   url: "https://www.geleiatotal.com.br/",
+  //   techs: [
+  //     {
+  //       icon: "wordpress",
+  //       name: "Wordpress",
+  //     },
+  //     {
+  //       icon: "php",
+  //       name: "PHP",
+  //     },
+  //     {
+  //       icon: "digitalocean",
+  //       name: "Digital Ocean",
+  //     },
+  //     {
+  //       icon: "pwa",
+  //       name: "PWA",
+  //     },
+  //   ],
+  // },
+  // {
+  //   imageUrl: "/mapa-aborto-legal.gif",
+  //   imageAlt: "Mapa Aborto Legal",
+  //   title: "Mapa Aborto Legal",
+  //   description:
+  //     "All about legal abortion in 🇧🇷 - What situations are allowed, what is needed, which hospitals to turn to and more information",
+  //   url: "https://mapaabortolegal.org/",
+  //   techs: [
+  //     {
+  //       icon: "wordpress",
+  //       name: "Wordpress",
+  //     },
+  //     {
+  //       icon: "php",
+  //       name: "PHP",
+  //     },
+  //   ],
+  // },
 ];
 
 export const posts = [
-  {
-    imageUrl: "/post-strapi-digital-ocean.webp",
-    imageAlt: "Deploying Strapi 4 to Digital Ocean App",
-    title: "Deploying Strapi 4 to Digital Ocean App",
-    description:
-      "Step by step on how to deploying Strapi 4 to Digital Ocean App",
-    url: "https://dev.to/candidosales/deploying-strapi-4-to-digital-ocean-app-1d8d",
-  },
-  {
-    imageUrl: "/post-strapi-railway.webp",
-    imageAlt: "Deploying Strapi 4 to Railway",
-    title: "Deploying Strapi 4 to Railway",
-    description: "Step by step on how to deploying Strapi 4 to Railway",
-    url: "https://dev.to/candidosales/deploying-strapi-4-to-railway-589b",
-  },
-  {
-    imageUrl: "/post-medicina.jpeg",
-    imageAlt: "O que a medicina pode aprender com a programação?",
-    title: "O que a medicina pode aprender com a programação?",
-    description:
-      "Esse texto são apenas reflexões que foram geradas durante diálogos entre um engenheiro de software e uma médica.",
-    url: "https://medium.com/@candidosales/o-que-a-medicina-pode-aprender-com-a-programa%C3%A7%C3%A3o-4ca71bd94fc4",
-  },
-  {
-    imageUrl: "/post-1.png",
-    imageAlt: "How I got a job offer in Canada",
-    title: "How I got a job offer in Canada",
-    description:
-      "In this article, I’ll present the process I did to get the job offer through VanHack",
-    url: "https://blog.usejournal.com/how-did-i-get-a-job-offer-in-canada-3351937653ad",
-  },
-  {
-    imageUrl: "/post-2.png",
-    imageAlt: "Como consegui uma oferta de emprego no Canadá",
-    title: "Como consegui uma oferta de emprego no Canadá",
-    description:
-      "Vou apresentar o processo que fiz para conseguir a oferta de emprego através da VanHack",
-    url: "https://medium.com/@candidosales/como-consegui-uma-oferta-de-emprego-no-canad%C3%A1-d8d8e717a379",
-  },
-  {
-    imageUrl: "/post-3.png",
-    imageAlt: "Como fiz o intercâmbio para o Canadá",
-    title: "Como fiz o intercâmbio para o Canadá",
-    description:
-      "Vou contar a minha experiência de como me mudei para o Canadá",
-    url: "https://medium.com/@candidosales/como-consegui-uma-oferta-de-emprego-no-canad%C3%A1-d8d8e717a379",
-  },
+  // {
+  //   imageUrl: "/post-strapi-digital-ocean.webp",
+  //   imageAlt: "Deploying Strapi 4 to Digital Ocean App",
+  //   title: "Deploying Strapi 4 to Digital Ocean App",
+  //   description:
+  //     "Step by step on how to deploying Strapi 4 to Digital Ocean App",
+  //   url: "https://dev.to/candidosales/deploying-strapi-4-to-digital-ocean-app-1d8d",
+  // },
+  // {
+  //   imageUrl: "/post-strapi-railway.webp",
+  //   imageAlt: "Deploying Strapi 4 to Railway",
+  //   title: "Deploying Strapi 4 to Railway",
+  //   description: "Step by step on how to deploying Strapi 4 to Railway",
+  //   url: "https://dev.to/candidosales/deploying-strapi-4-to-railway-589b",
+  // },
+  // {
+  //   imageUrl: "/post-medicina.jpeg",
+  //   imageAlt: "O que a medicina pode aprender com a programação?",
+  //   title: "O que a medicina pode aprender com a programação?",
+  //   description:
+  //     "Esse texto são apenas reflexões que foram geradas durante diálogos entre um engenheiro de software e uma médica.",
+  //   url: "https://medium.com/@candidosales/o-que-a-medicina-pode-aprender-com-a-programa%C3%A7%C3%A3o-4ca71bd94fc4",
+  // },
+  // {
+  //   imageUrl: "/post-1.png",
+  //   imageAlt: "How I got a job offer in Canada",
+  //   title: "How I got a job offer in Canada",
+  //   description:
+  //     "In this article, I’ll present the process I did to get the job offer through VanHack",
+  //   url: "https://blog.usejournal.com/how-did-i-get-a-job-offer-in-canada-3351937653ad",
+  // },
+  // {
+  //   imageUrl: "/post-2.png",
+  //   imageAlt: "Como consegui uma oferta de emprego no Canadá",
+  //   title: "Como consegui uma oferta de emprego no Canadá",
+  //   description:
+  //     "Vou apresentar o processo que fiz para conseguir a oferta de emprego através da VanHack",
+  //   url: "https://medium.com/@candidosales/como-consegui-uma-oferta-de-emprego-no-canad%C3%A1-d8d8e717a379",
+  // },
+  // {
+  //   imageUrl: "/post-3.png",
+  //   imageAlt: "Como fiz o intercâmbio para o Canadá",
+  //   title: "Como fiz o intercâmbio para o Canadá",
+  //   description:
+  //     "Vou contar a minha experiência de como me mudei para o Canadá",
+  //   url: "https://medium.com/@candidosales/como-consegui-uma-oferta-de-emprego-no-canad%C3%A1-d8d8e717a379",
+  // },
 ];
